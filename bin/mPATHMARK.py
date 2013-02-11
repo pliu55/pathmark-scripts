414ef298edab70644ef21241db895f5d4ad3eaa8
Sam Ng <OrosTheAvenger@eduroam-243-20.ucsc.edu>
2013-02-11 12:02:50 -0800

    changed mean/sd to median/mad

--- bin/mPATHMARK.py
+++ bin/mPATHMARK.py
@@ -253,6 +253,28 @@ def mean_std(inList, sample = True):
             std = 0.0
     return(mean, std)
 
+def median(inList):
+    i = len(inList)
+    if not i%2:
+        return (inList[(i/2)-1]+inList[i/2])/2.0
+    return inList[i/2]
+
+def median_mad(inList):
+    """Calculates median and mad"""
+    
+    cList = floatList(inList)
+    cList.sort()
+    if len(cList) == 0:
+        median = "NA"
+        mad = "NA"
+    else:
+        median = median(cList)
+        adList = []
+        for i in cList:
+            adList.append(abs(i-median))
+        mad = median(adList)
+    return(median, mad)
+    
 def r2Col(inf, appendData = {}, delim = "\t", null = "NA", header = False):
     """read 2 column data"""
     inData = deepcopy(appendData)
--- utilities/cytoscape-props.py
+++ utilities/cytoscape-props.py
@@ -11,10 +11,12 @@ o = open("all.props", "w")
 for line in f:
     if line.startswith("nodeCustomGraphics1.default-Node\ Custom\ Graphics\ 1-Discrete\ Mapper.mapping.map"):
         pline = re.split(",", line.rstrip("\r\n"))
+        print pline
         imgCounter = int(pline[1])
         for feature in features:
-            reline = re.sub(features[0], feature, pline[0]) + "," + str(imgCounter) + "," + re.sub(features[0], feature, pline[1]) + "," + pline[2]
+            reline = re.sub(features[0], feature, pline[0]) + "," + str(imgCounter) + "," + re.sub(re.sub("/", "_", features[0]), re.sub("/", "_", feature), pline[2]) + "," + pline[3]
             imgCounter += 1
+            o.write(reline+"\n")
     else:
         o.write(line)
 f.close()
