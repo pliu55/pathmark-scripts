5705c3181d89804b2f3dce79da64cfd887cf40d9
Sam Ng <OrosTheAvenger@Oros-the-Avenger.local>
2013-08-03 13:59:15 -0700

    outputs sif for sig test

--- bin/PATHMARK.py
+++ bin/PATHMARK.py
@@ -421,6 +421,10 @@ def PATHMARK(files, globalPathway, pathmarkFeatures = None, forcedStats = None,
         f = open("%s/%s_%s_nodrug.xgmml" % (currentDirectory, feature, filterString), "w")
         write_cytoscape_xgmml(pathmarkGraph, f, signedData[0][feature])
         f.close()
+        
+        f = open("%s/%s_%s_nodrug.xgmml" % (currentDirectory, feature, filterString), "w")
+        write_sif(pathmarkGraph, f)        
+        f.close()
 
 if __name__ == "__main__":
     ## parse arguments
